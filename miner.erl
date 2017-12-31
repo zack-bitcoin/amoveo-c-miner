239bb78ca1c59f949edcf6e29d08c26964d7285e
zack <zack.bitcoin@gmail.com>
2017-12-31 02:43:40 -0800

    fix bug where the miner would crash if it ever failed to connect to the mining pool even once.

--- miner.erl
+++ miner.erl
@@ -8,6 +8,7 @@
 -define(mode, pool).
 -define(Pubkey, <<"BHjaeLteq9drDIhp8d0R6JmUqkivIW1M0Yoh5rsGnw4wePMKowcNGHqfttAF52jMYhsZicFr7eIOWN/Sr0XI+OI=">>).
 -define(period, 10).%how long to wait in seconds before checking if new mining data is available.
+-define(pool_sleep_period, 1000).%How long to wait in miliseconds if we cannot connect to the mining pool.
 %This should probably be around 1/20th of the blocktime.
 
 
@@ -33,7 +34,7 @@ start2() ->
     kill_os_mains(),
     flush(),
     Data = <<"[\"mining_data\"]">>,
-    R = talk_helper(Data, ?Peer, 10),
+    R = talk_helper(Data, ?Peer),
     start_c_miners(R).
 start_c_miners(R) ->
     {F, _, Third} = unpack_mining_data(R), %S is the nonce
@@ -48,8 +49,7 @@ start_c_miners(R) ->
             {ok, <<Nonce:256>>} = file:read_file("nonce.txt"),
             BinNonce = base64:encode(<<Nonce:256>>),
             Data = << <<"[\"work\",\"">>/binary, BinNonce/binary, <<"\",\"">>/binary, ?Pubkey/binary, <<"\"]">>/binary>>,
-                                                %talk_helper2(Data, ?Peer),
-            talk_helper(Data, ?Peer, 40),%spend 8 seconds checking 5 times per second if we can start mining  again.
+            talk_helper(Data, ?Peer),
             io:fwrite("Found a block. 2\n"),
             timer:sleep(200)
     after (?period * 1000) ->
@@ -61,20 +61,18 @@ start_c_miners(R) ->
     start2().
 talk_helper2(Data, Peer) ->
     httpc:request(post, {Peer, [], "application/octet-stream", iolist_to_binary(Data)}, [{timeout, 3000}], []).
-talk_helper(Data, Peer, N) ->
-    if 
-        N == 0 -> 
-            io:fwrite("cannot connect to server"),
-            1=2;
-        true -> 
-            case talk_helper2(Data, Peer) of
-                {ok, {_Status, _Headers, []}} ->
-                    timer:sleep(200),
-                    talk_helper(Data, Peer, N - 1);
-                {ok, {_, _, R}} -> R;
-                _ -> io:fwrite("\nYou need to turn on and sync your Amoveo node before you can mine. You can get it here: https://github.com/zack-bitcoin/amoveo \n"),
-                     1=2
-            end
+talk_helper(Data, Peer) ->
+    case talk_helper2(Data, Peer) of
+        {ok, {_Status, _Headers, []}} ->
+            io:fwrite("server gave confusing response\n"),
+            timer:sleep(?pool_sleep_period),
+            talk_helper(Data, Peer);
+        {ok, {_, _, R}} -> R;
+        %{error, _} ->
+        E -> 
+            io:fwrite("\nIf you are running a solo-mining node, then this error may have happened because you need to turn on and sync your Amoveo node before you can mine. You can get it here: https://github.com/zack-bitcoin/amoveo \n If this error happens while connected to the public mining node, then it can probably be safely ignored."),
+             timer:sleep(?pool_sleep_period),
+             talk_helper(Data, Peer)
     end.
 slice(Bin, Char) ->
     slice(Bin, Char, 0).
